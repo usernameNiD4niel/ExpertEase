4a838a33feaa5082cce07c57d600d4cb08edb1fd
Daniel Rey <reyvillanuevadaniel@gmail.com>
2024-03-15 22:00:34 +0800

    addd

--- src/endpoints/post-customer.ts
+++ src/endpoints/post-customer.ts
@@ -6,6 +6,8 @@ export default async function postCustomer(customerData: AddCustomerType) {
 		{
 			headers: {
 				"Content-Type": "application/json",
+				"API-Key":
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMDUxMDY5NSwianRpIjoiMzA5NGYyOGMtN2E5ZS00MTdjLWEzMzAtNDU2NmNmMWE1ZWQ3IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ1c2VyX2lkIjo2LCJleHAiOiJTdW4sIDE0IEFwciAyMDI0IDEzOjUxOjM1IEdNVCJ9LCJuYmYiOjE3MTA1MTA2OTUsImV4cCI6MTcxMDUzOTQ5NX0.PF6RP1BHSF0PkQVcYBEmBvnDzTnv3DbTS89Kl227cHg",
 			},
 			method: "POST",
 			body: JSON.stringify(customerData),
